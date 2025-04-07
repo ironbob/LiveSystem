4b34924112d0d17ed62ed7c38e04bb9aea95c35d
ironbob <422049073@qq.com>
2025-04-07 08:52:52 +0800

    直播系统running初步代码

--- src/main/kotlin/com/wtb/livesystem/execute/AppInstance.kt
+++ src/main/kotlin/com/wtb/livesystem/execute/AppInstance.kt
@@ -2,6 +2,7 @@ package com.wtb.livesystem.execute
 
 import com.wtb.livesystem.config.app.model.App
 import com.wtb.livesystem.config.auth.SecurityConfig
+import com.wtb.livesystem.core.*
 import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.time.LocalDateTime
@@ -13,11 +14,57 @@ class AppInstance(private val app: App) {
     private val speechQueue: Queue<String> = LinkedList()
     private var startTime: LocalDateTime? = null
 
+    private val roomStatus: RoomStatus = RoomStatus(0, hashSetOf(), arrayListOf())
+    private val streamerState: StreamerState = StreamerState(null, null, 0)
+
+    private val processedActions = arrayListOf<ProcessAction>()
+
+
+    data class ProcessAction(val replyContent: SpeechReply, val timeStamp: Long, val processEvents: List<LiveEvent>, val actionType:ActionType)
+
+    /**
+     * 读稿、暖场、回复
+     */
+    enum class ActionType{
+        ReadDraft,Warmup,ReplyAudience
+    }
+
+    data class Draft(val sentences: List<String>, var warmups:List<String>, )
+
+    class DraftPlayState(var currentDraft:Draft)
+
+
+    private val eventListLock = Any()
+    /**
+     * 新的用户消息
+     */
+    fun onNewLiveEvent(liveEvent: LiveEvent){
+        synchronized(eventListLock) {
+            roomStatus.pendingEvents.add(liveEvent)
+        }
+    }
+
+    /**
+     * 更新房间状态
+     */
+    fun onNewRoomStatus(status: RoomStatusUpdateMessage){
+        roomStatus.onlineUserCount = status.onlineUserCount;
+        roomStatus.activeUserIds = status.activeUserIds
+    }
+
+    /**
+     *
+     */
+    fun fetchNextSpeechReply():SpeechReply?{
+        return null
+
+    }
 
     init {
 
         this.startTime = LocalDateTime.now()
     }
+
     fun getApp(): App {
         return app
     }
@@ -33,11 +80,13 @@ class AppInstance(private val app: App) {
         val seconds: Int = duration.toSecondsPart()
         return String.format("%d小时 %d分钟 %d秒", hours, minutes, seconds)
     }
+
     init {
         speechQueue.add("你好，我是你的虚拟主播！")
         speechQueue.add("接下来，我们继续进行直播...")
         speechQueue.add("欢迎新的观众加入直播间！")
     }
+
     val logger = LoggerFactory.getLogger(AppInstance::class.java)
     fun start() {
         running = true
